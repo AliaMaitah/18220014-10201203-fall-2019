6388dfd57b81e7cefe3bea56d03b3b176cd48942
AliaMaitah <56788215+AliaMaitah@users.noreply.github.com>
2019-10-27 14:31:35 +0200

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add
+{
+   public static void main( String args[]){
+   
+   int num1=4;
+   int num2=5;
+   int sum= num1+num2;
+   System.out.println("sum="+sum);
+   }
+   }
+   
+   
